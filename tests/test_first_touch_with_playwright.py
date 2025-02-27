422ad36e41666068beb2454c8f7f0a5d4c5ccb4b
v.borys <v.borys@paytechlab.com>
2025-02-27 22:13:52 +0200

    update

--- tests/test_first_touch_with_playwright.py
+++ tests/test_first_touch_with_playwright.py
@@ -2,13 +2,15 @@ from playwright.sync_api import Page, expect
 
 # pytest --headed
 
-def test_one(page: Page):
+def test_one(browser):
+    page = browser.new_page()
+    page.goto('https://www.pravda.com.ua/news/2025/02/26/7500292/')
     page.goto('https://www.pravda.com.ua/news/2025/02/26/7500246/')
     expect(page.get_by_text('У Румунії заарештували проросійського екскандидата у президенти').nth(0)).to_be_visible()
 
 
-def test_second(page: Page):
+def test_second(browser):
+    page = browser.new_page()
     page.goto('https://www.pravda.com.ua/news/2025/02/26/7500292/')
-
     page.locator('div.post_text > p:nth-child(2) > a').click()
     expect(page.locator("#endless7205944")).to_have_text("повідомило")
