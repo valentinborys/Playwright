2559658b1ec51258767acb52282909b5543149a9
v.borys <v.borys@paytechlab.com>
2025-03-01 00:29:17 +0200

    test_project_manegmant_registration

--- tests/test_playwright_codegen.py
+++ tests/test_playwright_codegen.py
@@ -24,12 +24,17 @@ def test_playwright_codegen(browser) -> None:
 
 
 def test_project_manegmant_registration(browser):
+
+    # Create a new browser context
     context = browser.new_context()
     page = context.new_page()
     pl_codegen = PlaywrightPage()
 
+    # Open the target page
     page.goto("https://ithillel.ua/")
 
+    # Fill out and submit the form
     pl_codegen.fill_project_manegmant_registration(page)
 
+    # Verify that the success message is displayed
     expect(page.get_by_text("Ви ввели невірну адресу електронної пошти або пароль")).to_have_text('Ви ввели невірну адресу електронної пошти або пароль')
